a0418003721443f81395a46be36344a363b6a8bf
jamilsonh <2jamilson@gmail.com>
2023-03-21 17:29:56 -0300

    Adicionando remoção de dados da localstorage por ID

--- src/ListOfContacts.jsx
+++ src/ListOfContacts.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import './App.css';
 import './ListOfContacts.css';
 
-const ListOfContacts = ( ) => {
+const ListOfContacts = () => {
   const [contacts, setContacts] = useState([]);
 
   function limparStorage() {
-    localStorage.removeItem('contacts') 
+    localStorage.removeItem('contacts');
     const savedContacts = JSON.parse(localStorage.getItem('contacts')) || [];
     setContacts(savedContacts);
   }
@@ -17,55 +17,58 @@ const ListOfContacts = ( ) => {
     setContacts(savedContacts);
   }, []);
 
+  function removerContato(ctRemover) {
+    const tmp = contacts.filter((ct) => ct.firstName !== ctRemover.firstName);
+    //setContacts([...contacts, newContact]);
+    setContacts(tmp);
+    localStorage.setItem('contacts', JSON.stringify(tmp));
+  }
+
+  console.log(contacts);
+
   return (
-    <div className="container-main"> 
-    <div>CONTACT BOOK</div>
-      <div className="sub-container">  
+    <div className='container-main'>
+      <div>CONTACT BOOK</div>
+      <div className='sub-container'>
         <div className='buttons'>
-          <button
-            className="save-button"    
-          >
-            <Link
-              className='link' 
-              to={'/addcontact'}
-            >
+          <button className='save-button'>
+            <Link className='link' to={'/addcontact'}>
               Add Contact
-            </Link>            
+            </Link>
           </button>
-          <button 
-            className="save-button" 
-            onClick={limparStorage}
-          >
+          <button className='save-button' onClick={limparStorage}>
             Clear Contacts
           </button>
         </div>
 
         <div className='contacts-list'>
-          <div className='contacts-title'>
-            Contacts
-          </div>
+          <div className='contacts-title'>Contacts</div>
           {contacts.map((item, index) => (
-              <div key={index} className="contacts-card">
-                <div className="contact-name">
-                  <div className="first-name">{item.firstName}</div>
-                  <div className="last-name">{item.lastName}</div>
-                </div>
-                <div className="contact-button">
-                  <button
-                    className="details-button"
-                  >
-                    <Link 
-                      className='link'
-                      to={`/details/${item.firstName}`
-                    }>
-                      Details
-                    </Link>
-                  </button>   
-                </div>
-              </div>                    
+            <div key={index} className='contacts-card'>
+              <div className='contact-name'>
+                <div className='first-name'>{item.firstName}</div>
+                <div className='last-name'>{item.lastName}</div>
+              </div>
+              <div className='contact-button'>
+                <button
+                  onClick={() => {
+                    removerContato({
+                      firstName: item.firstName,
+                    });
+                  }}
+                >
+                  DELETAR
+                </button>
+                <button className='details-button'>
+                  <Link className='link' to={`/details/${item.firstName}`}>
+                    Details
+                  </Link>
+                </button>
+              </div>
+            </div>
           ))}
         </div>
-      </div>  
+      </div>
     </div>
   );
 };

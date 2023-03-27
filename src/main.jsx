438476c589e56df8f6f9f95dbccafb536bd63816
jamilsonh <2jamilson@gmail.com>
2023-03-27 19:14:04 -0300

    Corrigindo Bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import App from './App'
-import Contacts from './AddContacts'
-import Details from './Details'
-import './index.css'
-import ListOfContacts from './ListOfContacts'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App';
+import Contacts from './AddContacts';
+import Details from './Details';
+import './index.css';
+import ListOfContacts from './ListOfContacts';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<App />}>
-          <Route path="/" element={<ListOfContacts />} />
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/addcontact" element={<Contacts />} />
+          <Route path='/' element={<ListOfContacts />} />
+          <Route path='details/:id' element={<Details />} />
+          <Route path='addcontact' element={<Contacts />} />
         </Route>
-      </Routes> 
+      </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
